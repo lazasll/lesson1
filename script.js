7e505bf61adc47a65f79d48841dfa2e47d06384d
lazasll <62263195+lazasll@users.noreply.github.com>
2020-08-25 14:28:32 +0300

    После разбора на уроке сделала свое

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-const goods = [
+const products = [
   { title: 'Shirt', price: 150 },
   { title: 'Socks', price: 50 },
   { title: 'Jacket', price: 350 },
   { title: 'Shoes', price: 250 },
+  { title: 'Sweater', price: 450 },
+  { title: 'T-Shirt', price: 175 },
+  { title: 'Jacket', price: 550 },
+  { title: 'Shoes', price: 750 },
+  { title1: 'Shoes', price1: 950 },
 ];
 
-const renderGoodsItem = (title, price) => {
-  return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+const renderProductItem = (title = 'product', price = 0) => {
+  return `
+            <div class="product">
+              <a href="single__page.html"><img class="product__img" src="img/men/1937.png" alt="photo"></a>
+                  <div class="product__content">
+                        <a href="#" class="product__name">${title}</a>
+                        <p class="product__price">$${price}</p>
+                  </div>
+                        <a href="#" class="product__add">Add to Cart</a>
+                        <a href="#" class="product__add__XX"><img class="" src="img/men/1895.png"></a>
+                        <a href="#" class="product__add__love"><img class="" src="img/men/1899.png"></a>
+            </div>
+        `;
 };
 
-const renderGoodsList = (list) => {
-  const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-  document.querySelector('.goods-list').innerHTML = goodsList.join('');
-}
+const renderProductList = (list = [{ title: 'product', price: 0 }]) => {
+  let productList = list.map(({ title, price }) => renderProductItem(title, price));
+  document.querySelector('.product__box').innerHTML = productList.join(' ');
 
-renderGoodsList(goods);
+}
+renderProductList(products)
